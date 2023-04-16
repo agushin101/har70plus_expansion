79cf4488550029b620d4909d3ffb590491ce9c30
agushin101 <83178075+agushin101@users.noreply.github.com>
2023-04-15 22:36:18 -0400

    Update concat.py

--- concat.py
+++ concat.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 
 def main():
     final_x = pd.DataFrame()
@@ -11,9 +10,6 @@ def main():
 
         final_x = pd.concat([final_x, x], axis=0)
         final_y = pd.concat([final_y, y], axis=0)
-
-    scaler = MinMaxScaler().set_output(transform='pandas')
-    final_x = scaler.fit_transform(final_x)
 
     final_x.to_csv("x.csv", index=False)
     final_y.to_csv("y.csv", index=False)
